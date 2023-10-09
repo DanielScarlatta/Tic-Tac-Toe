c3314f31659227bc85773a707d625398b2573ff7
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2023-10-09 06:38:50 -0300

    praticas

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const matriz = [
   [],
   []
 ]
+let contPlayer = 'X'
+let winner = ''
+let varietWinner = ''
 const buttonsSelect = document.querySelectorAll(".boardOptions")
-let contPlayer = 0
+const  board = document.getElementById("gameBoard")
+const restartGame = document.getElementById("restartGame")
 
 function startNamePlayer() {
   const namePlayerX = window.prompt("Qual será nome do primeiro jogador (X): ")
@@ -12,68 +16,120 @@ function startNamePlayer() {
   const playerX = document.getElementById("playerX")
   const playerO = document.getElementById("playerO")
 
-  if(namePlayerX !== "") {
+  if (namePlayerX !== "") {
     playerX.innerText = namePlayerX + " [ X ] "
   }
-  if(namePlayerO !== ""){
+  if (namePlayerO !== "") {
     playerO.innerText = namePlayerO + " [ O ] "
   }
 }
 
-function checkerResult() {
-  for(let l=0; l<=2; l++){
-    for(let c=0; c<=2; c++){
-      if(matriz[l][c] == matriz[l][c]){
-        console.log("ganhou")
-      }
-    }
+function checkerResult(varietWinner) {
+  let lineWinner = []
+  if (matriz[0][0] == varietWinner && matriz[0][1] == varietWinner && matriz[0][2] == varietWinner) {
+    lineWinner = [optionBoard1, optionBoard2, optionBoard3]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    return true
+  }
+  if (matriz[1][0] == varietWinner && matriz[1][1] == varietWinner && matriz[1][2] == varietWinner) {
+    lineWinner = [optionBoard4, optionBoard5, optionBoard6]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  if (matriz[2][0] == varietWinner && matriz[2][1] == varietWinner && matriz[2][2] == varietWinner) {
+    lineWinner = [optionBoard7, optionBoard8, optionBoard9]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  //---------------------------------------
+  if (matriz[0][0] == varietWinner && matriz[1][0] == varietWinner && matriz[2][0] == varietWinner) {
+    lineWinner = [optionBoard1, optionBoard4, optionBoard7]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  if (matriz[0][1] == varietWinner && matriz[1][1] == varietWinner && matriz[2][1] == varietWinner) {
+    lineWinner = [optionBoard2, optionBoard5, optionBoard8]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  if (matriz[0][2] == varietWinner && matriz[1][2] == varietWinner && matriz[2][2] == varietWinner) {
+    lineWinner = [optionBoard3, optionBoard6, optionBoard9]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  //--------------------------------------
+  if (matriz[0][0] == varietWinner && matriz[1][1] == varietWinner && matriz[2][2] == varietWinner) {
+    lineWinner = [optionBoard1, optionBoard5, optionBoard9]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
+  }
+  if (matriz[0][2] == varietWinner && matriz[1][1] == varietWinner && matriz[2][0] == varietWinner) {
+    lineWinner = [optionBoard3, optionBoard5, optionBoard7]
+    lineWinner.forEach(function(ele) {
+      ele.classList.add("winnwePlayer")
+    })
+    console.log("ganhou " + varietWinner)
+    return true
   }
 }
 
+function disebleBoard() {
+  buttonsSelect.forEach(function(ele){
+    ele.classList.add("diseble")
+  })
+}
+
 function buttonChecker() {
-  buttonsSelect.forEach(function(ele) {
-    ele.addEventListener("click", function() {
+  buttonsSelect.forEach(function (ele) {
+    ele.addEventListener("click", function () {
       const positionMatrizPrimary = ele.dataset.primary
       const positionMatrizSegundary = ele.dataset.segundary
 
-      if(contPlayer == 0 && matriz[positionMatrizPrimary][positionMatrizSegundary] == undefined) {
-        ele.innerText = "X"
-        matriz[positionMatrizPrimary][positionMatrizSegundary] = "X"
-        contPlayer++
-      }else if(contPlayer == 1 && matriz[positionMatrizPrimary][positionMatrizSegundary] == undefined) {
-        ele.innerText = "O"
-        contPlayer--
+      if (contPlayer == 'X' && ele.innerText !== 'O' && matriz[positionMatrizPrimary][positionMatrizSegundary] == undefined) {
+        ele.innerText = 'X'
+        matriz[positionMatrizPrimary][positionMatrizSegundary] = 'X'
+        contPlayer = 'O'
+        varietWinner = 'X'
+        if(checkerResult(varietWinner)) {
+          disebleBoard()
+        }
+
+      } else if (contPlayer == 'O' && ele.innerText !== 'X' && matriz[positionMatrizPrimary][positionMatrizSegundary] == undefined) {
+        ele.innerText = 'O'
+        matriz[positionMatrizPrimary][positionMatrizSegundary] = 'O'
+        contPlayer = 'X'
+        varietWinner = 'O'
+        if(checkerResult(varietWinner)) {
+          disebleBoard()
+        }
       }
     })
   })
 }
 
-
-// function buttonChecker() {
-//   buttonsSelect.forEach(function(ele) {
-//     ele.addEventListener("click", function() {
-//       let positionMatriz = ele.dataset.value
-  
-//       if(contPlayer == 0 && matriz[positionMatriz] == undefined) {
-//         ele.innerText = "X"
-//         matriz[ele.dataset.primary][ele.dataset.segundary] = "X"
-//         contPlayer++
-//         //vai chamar uma func para verificar se tem ganhador
-
-//       } else if(contPlayer == 1 && matriz[positionMatriz] == undefined){
-//         ele.innerText = "O"
-//         matriz[ele.dataset.primary][ele.dataset.segundary] = "O"
-//         contPlayer--
-//         //vai chamar uma func para verificar se tem ganhador
-//         //pistas os botoes de vermelho o ganhador
-//       }
-//       checkerResult()
-//       console.log(matriz)
-      
-//     })
-//   })
-// }
-
-
 startNamePlayer()
 buttonChecker()
+
+restartGame.addEventListener('click', function() {
+  window.location.reload()
+})
