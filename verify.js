e58ef2c4ee1fe9e24ea577b5189e66e5578c51c2
Daniel Dias Scarlatta <daniel.scarlatta@capitalconsig.local>
2023-11-07 13:32:17 -0300

    praticas

--- verify.js
+++ verify.js
@@ -7,12 +7,15 @@ const board = [
 
 // variaveis do jogo
 let currentPlayer = 'X'
+let Player = 'O'
 const playerWinner = ''
+const buttonsList = document.querySelectorAll('button[class="boardOptions"]')
 const namePlayerX = document.getElementById('playerX')
 const namePlayerO = document.getElementById('playerO')
 const playerX = window.prompt('Digite o nome do jogador X')
 const playerO = window.prompt('Digite o nome do jogador O')
 const gameResult = ''
+
 
 // pegando os dados do tabuleiro
 
@@ -32,7 +35,7 @@ conteiner.addEventListener('click', (event) => {
 function playerMoment() {
   const namePlayerX = document.getElementById('playerX')
   const namePlayerO = document.getElementById('playerO')
-  
+
   namePlayerX.innerText = playerX + ' [X] '
   namePlayerO.innerText = playerO + ' [O] '
 }
@@ -42,13 +45,17 @@ playerMoment()
 function runGame() {
   // criar as verificações e lógica do jogo aqui
   console.log(board)
+
   function addToBoard() {
-    if(currentPlayer === 'X' && board[dataButton.primary][dataButton.segundary] === '') {
+    Player = 'X'
+    if (currentPlayer === 'X' && board[dataButton.primary][dataButton.segundary] === '') {
       board[dataButton.primary][dataButton.segundary] = currentPlayer
       document.getElementById(dataButton.id).innerText = currentPlayer
+
       return currentPlayer = 'O'
-    } 
-    if(currentPlayer === 'O' && board[dataButton.primary][dataButton.segundary] === '') {
+    }
+    Player = 'O'
+    if (currentPlayer === 'O' && board[dataButton.primary][dataButton.segundary] === '') {
       board[dataButton.primary][dataButton.segundary] = currentPlayer
       document.getElementById(dataButton.id).innerText = currentPlayer
 
@@ -56,14 +63,49 @@ function runGame() {
     }
   }
   addToBoard()
-  
+  resultCheck()
+
   function resultCheck() {
-    for(let l=0; l<3; l++) {
-      if(board[l][0] == board[l][1] == board[l][2] && ) {
-        let winner = [board[l][0], board[l][1], board[l][2]]
-        console.log('vencedor' + board[l][0])
+    function block() {
+      buttonsList.forEach(function (ele) {
+        ele.classList.add('diseble')
+      })
+    }
+
+    for (let l = 0; l < 3; l++) {
+      if (board[l][0] == Player && board[l][1] == Player && board[l][2] == Player) {
+        console.log("vencedor " + Player)
+        block()
+        lineWinner = [optionBoard1, optionBoard2, optionBoard3]
+        lineWinner.forEach(function (ele) {
+          ele.classList.add("winnwePlayer")
+        })
+      }
+
+      if (board[0][l] == Player && board[1][l] == Player && board[2][l] == Player) {
+        console.log("vencedor " + Player)
+        block()
+        lineWinner = [optionBoard1, optionBoard2, optionBoard3]
+        lineWinner.forEach(function (ele) {
+          ele.classList.add("winnwePlayer")
+        })
+      }
+      if (board[0][0] == Player && board[1][1] == Player && board[2][2] == Player) {
+        console.log("vencedor " + Player)
+        block()
+        lineWinner = [optionBoard1, optionBoard2, optionBoard3]
+        lineWinner.forEach(function (ele) {
+          ele.classList.add("winnwePlayer")
+        })
+      }
+      if (board[0][2] == Player && board[1][1] == Player && board[2][0] == Player) {
+        console.log("vencedor " + Player)
+        block()
+        lineWinner = [optionBoard1, optionBoard2, optionBoard3]
+        lineWinner.forEach(function (ele) {
+          ele.classList.add("winnwePlayer")
+        })
       }
     }
   }
-  resultCheck()
 }
